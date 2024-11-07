8131a8811188b0a62f4f3cd7647e08929be1a758
Hrishikesh Pardeshi <hrishikeshpardeshi@Hrishikeshs-iMac.local>
2024-11-07 13:11:23 +0530

    remove admin from middleware

--- pdf_reader/settings.py
+++ pdf_reader/settings.py
@@ -32,14 +32,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pdf_processor',
     'rest_framework',
+    'pdf_processor',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
